2ec83dd28bdf011c8c4943d27625f2e7887b91af
priyanka-arrk <priyanka.patil@arrkgroup.com>
2016-02-11 17:20:38 +0000

    program for performing add, delete , update and display operations on employee details

--- employee_information.rb
+++ employee_information.rb
@@ -1,3 +1,8 @@
+=begin
+ Program for performing operations on employee details
+  Author: Priyanka Patil
+ Date Created: 05/02/2016
+=end
 class Employee
 	attr_accessor :choice, :employee_id,:employee_name,:employee_designation,:employee_doj,:employee_salary,:employee_experience,:employee_contact_no,:office_email
 	puts "==========================="
@@ -37,7 +42,7 @@ class Employee
 		when 4
 			delete_employee
 		else
-			puts "Sorry! You entered Wrong Choice"
+			puts "Sorry! You Entered Wrong Choice"
 		end 	#end of case statement
   	end while @choice < 5
 	end 	#end of operations method
@@ -70,14 +75,24 @@ class Employee
 	  @emp[:employee_salary]=@employee_salary
 	  @emp[:employee_experience]=@employee_experience
 	  @emp[:employee_contact_no]=@employee_contact_no
-	  @emp[:office_email]=@ffice_email
+	  @emp[:office_email]=@office_email
 	end #end of add employee method
 	def display_employee
-			puts "enter the employee id for which you want to see deatails"
+		puts "============================== "
+	  puts "DELETE EMPLOYEE INFORMATION"
+	  puts "============================== "
+			puts "enter The Employee id For Which You Want To See Details"
 			@id= gets.chomp.to_i
-			#a= @emp_hash.each_pair{|key,hash| hash[:employee_id]==@id}
 			a= @emp_hash.select{|key,hash| hash[:employee_id]==@id}
-			a.select {|key,value| puts "#{value}" }
+			a.select {|key,hash| 
+				puts "Employee id : #{hash[:employee_id]}"
+				puts "Employee Name : #{hash[:employee_name]}"
+	 			puts "Designation : #{hash[:employee_designation]}"
+	  		puts "Date of Joining : #{hash[:employee_doj]}"
+	  		puts "Employee Salary : #{hash[:employee_salary]}"
+	  		puts "Experience in years : #{hash[:employee_experience]}"
+	  		puts "Contact Number : #{hash[:employee_contact_no]}" 
+	 			puts "Employee's email-id : #{hash[:office_email]}"}
 	end
 	def delete_employee
 		puts "enter the employee id to delete employee information"
@@ -86,9 +101,75 @@ class Employee
 		puts " Employee #{@id} is deleted"
 	end 	#end of delete_employee method
 	def update_employee
-		#method definition
-	end
+		puts "============================== "
+	  puts "UPDATE EMPLOYEE INFORMATION"
+	  puts "============================== "
+		puts "Enter The Employee id For Which You Want To Update Details"
+		@id= gets.chomp.to_i
+		puts " Which Information do you want to update"
+		puts "1. Employee name"
+		puts "2. Employee Designation"
+		puts "3. Date of Joining"
+		puts "4. Salary"
+		puts "5. Experience"
+		puts "6. Contact Number"
+		puts "7. Office Email"
+		info = gets.chomp.to_i
+		case info
+			when 1
+				puts "enter the new name"
+				name = gets.chomp
+				@emp_hash.each do |key,hash| 
+					hash[:employee_name]= name if hash[:employee_id]==@id
+					puts hash
+				end
+				puts "Name of employee #{@id} is updated #{name}"
+			when 2
+				puts "Enter new Designation"
+				designation = gets.chomp
+				@emp_hash.each do |key,hash| 
+					hash[:employee_designation] = designation if hash[:employee_id]==@id
+				end
+				puts "Designation of employee #{@id} is updated to #{designation}"
+			when 3
+				puts "Enter new Date of Joining"
+				doj=gets.chomp
+				@emp_hash.each do |key,hash| 
+					hash[:employee_doj]= doj if hash[:employee_id]==@id
+				end
+				puts "Date of Joining of employee #{@id} is updated to #{doj}"
+			when 4
+				puts "Enter new salary"
+				salary=gets.chomp.to_i
+				@emp_hash.each do |key,hash| 
+					hash[:employee_salary]= salary if hash[:employee_id]==@id
+				end
+				puts "Salary of employee #{@id} is updated to  #{salary}"
+			when 5
+				puts "Enter new Experience in years"
+				experience=gets.chomp.to_i
+				@emp_hash.each do |key,hash| 
+					hash[:employee_experience]= experience if hash[:employee_id]==@id
+				end
+				puts "Experience of employee #{@id} is updated to  #{experience}"
+			when 6
+				puts "Enter new Contact Number"
+				contact_no=gets.chomp.to_i
+				@emp_hash.each do |key,hash| 
+					hash[:employee_contact_no]= contact_no if hash[:employee_id]==@id
+				end
+				puts "Contact Number of employee #{@id} is updated to  #{contact_no}"
+			when 7
+				puts "Enter new email-id"
+				email=gets.chomp
+				@emp_hash.each do |key,hash| 
+					hash[office_email]= email if hash[:employee_id]==@id
+				end
+				puts "Email id of employee #{@id} is updated to  #{email}"
+			else 
+				puts "You Entered Wrong Choice"
+			end
+	end 	#end of update_employee method
 end 	#end of class									
-
 employee= Employee.new
 employee.operations
